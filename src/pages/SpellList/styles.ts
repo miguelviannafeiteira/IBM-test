01b81deb3a713def42dbd927c8f200acca215112
Miguel Vianna <email@gmail.com>
2022-05-16 10:06:23 -0300

    codigo completo

--- src/pages/SpellList/styles.ts
+++ src/pages/SpellList/styles.ts
@@ -30,17 +30,26 @@ width:500px;
 display:flex;
 justify-content:space-between;
 align-items:center;
-
 background-color:rgba(244,243,245, .6);
 box-shadow: 0 0 60px rgba(0,0,0,.1);
 padding-left:10px;
 padding-right:10px;
 border-radius:5px;
 
+@media (max-width: 520px) {
+  flex-direction:column;
+  justify-content:center;
+  width:300px;
+  height:120px;
+  gap:15px;
+}
+
 .button {
   width:220px;
   margin:0;
 }
+
+
 `
 
 export const List = styled.div`
@@ -49,6 +58,12 @@ box-shadow: 0 0 60px rgba(0,0,0,.1);
 width:500px;
 margin-top:20px;
 border-radius:5px;
+
+@media (max-width: 520px) {
+  width:300px;
+  gap:15px;
+}
+
 ul {
   li ~ li {
   border-top: 1px solid rgba(119, 122, 146, 0.3);
